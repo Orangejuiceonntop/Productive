3d485ad0e6b8877a054f2f6b3bf741abc3304458
Orangejuiceonntop <susselkikijimenez@gmail.com>
2026-06-18 00:42:51 +0000

    Create quote generator

--- script.js
+++ script.js
@@ -42,3 +42,17 @@ inputBox.addEventListener("keypress", function(event){
         addTask();
     }
 });
+
+const quote = document.getElementById("quote")
+const author = document.getElementById("author")
+const api_url = "https://motivational-spark-api.vercel.app/api/quotes/random";
+
+async function getquote(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    quote.innerHTML = data.quote;
+    author.innerHTML = data.author;
+}
+
+getquote(api_url)
