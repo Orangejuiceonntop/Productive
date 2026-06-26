9db5ee35cd6da52a358fafc155c76277bebd9d37
Orangejuiceonntop <susselkikijimenez@gmail.com>
2026-06-26 03:36:56 +0000

    Create an array for the songs

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Frank Ocean - Lost",
+        artist: "Frank Ocean",
+        img: "",
+        src: "",
+    },
+    {
+        name: "Frank Ocean - Lost",
+        artist: "Frank Ocean",
+        img: "",
+        src: "",
+    },
+    {
+        name: "Frank Ocean - Lost",
+        artist: "Frank Ocean",
+        img: "",
+        src: "",
+    },
+    {
+        name: "Frank Ocean - Lost",
+        artist: "Frank Ocean",
+        img: "",
+        src: "",
+    },
+    {
+        name: "Frank Ocean - Lost",
+        artist: "Frank Ocean",
+        img: "",
+        src: "",
+    },
+]
