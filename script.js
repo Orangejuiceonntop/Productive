e4b302f3606077e3709e41187f7823a8bd99a81b
orangejuiceontop <susselkikijimenez@gmail.com>
2026-06-27 17:05:34 -0500

    Make calendar show current month + year

--- script.js
+++ script.js
@@ -159,4 +159,15 @@ minusmin.addEventListener("click", () => {
     timer.innerHTML = `${minCount --}:00`
 })
 
+const currentDate = document.querySelector(".current-date");
 
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const renderCalendar = () => {
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+}
+renderCalendar();
